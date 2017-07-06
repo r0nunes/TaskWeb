b398df7f5267ede1fb25256f450312dd2c2ddd7c
r0nunes <ronune.s@hotmail.com>
2017-07-05 23:17:25 -0300

    Include new scenario
    News tests

--- cucumber/features/specifications/taskList.feature
+++ cucumber/features/specifications/taskList.feature
@@ -1,10 +1,22 @@
 Feature: Task list 
 
 Background: 
-Given I access the Task Manager site
-When click in Task Manager
+    Given I access the Task Manager site
+    When click in Task Manager
 
 Scenario: Register a new task    
     And I fill in the field
     And click in Add Task
-    Then my registration will be successfully made
+    Then my task will be successfully actived
+
+Scenario: Finish task created
+    And I click in complete task
+    Then verify status task
+
+Scenario: Acitve task again
+    And I click in active task
+    Then my task will be successfully actived
+
+Scenario: Delete task
+    And I click in delete task
+    Then my task will be successfully deleted
--- cucumber/features/step_definitions/taskList_steps.rb
+++ cucumber/features/step_definitions/taskList_steps.rb
@@ -14,7 +14,30 @@ And(/^click in Add Task$/) do
     click_on "add-btn"
 end
 
-Then(/^my registration will be successfully made$/) do
+Then(/^my task will be successfully actived$/) do
   page.has_content?("Aprendendo Capybara")
+  page.find(:xpath,"//div[1]/ul/p/button[2]")
 end
-    
+
+And(/^I click in complete task$/) do
+    page.find(:xpath,"//div[1]/ul/p/button[2]").click
+end
+
+Then(/^verify status task$/) do
+    page.find(:xpath,"//div/ul/p/button[1]")
+end 
+
+And(/^I click in active task$/) do 
+    page.find(:xpath,"//div/ul/p/button[1]").click
+end 
+
+And(/^I click in delete task$/) do 
+    click_on "task_del_row_"
+end 
+
+And(/^my task will be successfully deleted$/) do 
+    assert has_no_field?('Aprendendo Capybara')
+end 
+
+
+
